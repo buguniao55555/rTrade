c9955df950313e4139ae63fcb550ec7bcb74c1bf
Felix <huj15@rpi.edu>
2022-06-20 18:53:18 -0400

    react build v1.0

---
+++ r-trade/src/App.js
@@ -0,0 +1,20 @@
+import logo from './rTrade_logo_darkMode.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} width={450} height={200} alt="logo" />
+        <p>
+          Welcome To rTrade
+        </p>
+        
+          an RCOS project
+        
+      </header>
+    </div>
+  );
+}
+ 
+export default App;
